663427fcd060e4af11bfc662a968f0b666800d01
s0uths1de <s0uths1de@hotmail.com>
2024-03-31 18:48:43 +0800

    0.3版本大部分功能实现

--- src/main/java/top/s0uths1de/Main.java
+++ src/main/java/top/s0uths1de/Main.java
@@ -4,25 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import top.s0uths1de.controller.ControllerMain;
-import top.s0uths1de.entity.FileEntity;
-import top.s0uths1de.function.SetButton;
+
 import top.s0uths1de.tools.Simplify;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class Main extends Application {
-
-    static FileEntity fe;
-    static List<List> lists;
-
     public static Stage stage;
 
     @Override
@@ -32,10 +24,6 @@ public class Main extends Application {
         List<Node> nodes = root.getChildren().subList(0, 1);
         Pane subscene = new StackPane();
         subscene.getChildren().addAll(nodes);
-//        ((Button) (subscene.lookup("#start"))).setOnAction(event -> {
-//            fe = ControllerMain.getFe();
-//            SetButton.setStart(this, primaryStage, fe);
-//        });
         primaryStage.setScene(new Scene(subscene, ComparatorValue.WIDTH, ComparatorValue.HEIGHT));
         Image image = new Image(Simplify.urlToString(this.getClass(), "/top/s0uths1de/filecomparator/assets/icon.png").toString());
         primaryStage.getIcons().setAll(image);
@@ -43,10 +31,6 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
         stage=primaryStage;
-    }
-
-    public static List getList() {
-        return lists;
     }
 
     public static void main(String[] args) {
--- src/main/java/top/s0uths1de/controller/ControllerMain.java
+++ src/main/java/top/s0uths1de/controller/ControllerMain.java
@@ -1,27 +1,16 @@
 package top.s0uths1de.controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.util.Callback;
-import top.s0uths1de.Main;
 import top.s0uths1de.entity.FileEntity;
 import top.s0uths1de.function.SetButton;
 
